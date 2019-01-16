01a95ce1782bdc86fa839d2b59bf595096e6d484
tobinjosh <46658226+tobinjosh@users.noreply.github.com>
2019-01-15 20:41:48 -0600

    Update equation_balancer.py
    added formula class

--- equation_balancer.py
+++ equation_balancer.py
@@ -1,37 +1,59 @@
 # Program that takes chemical equations and balances them
 
-
-
-
-
-
-
-
-
-
+class Formula():
+	"""Class that manipulates input chemical formulas"""
+	
+	def __init__(self, formula):
+		"""initializes formula and blank coefficient"""
+		self.formula = formula
+		self.coeff = ''
+		
+	def increase_coeff(self):
+		"""increases coefficient by 1"""
+		self.coeff += 1
+		
+	def formula_dict(self):
+		"""takes the formula and creates a dictionary of elements a values
+		input: string of each formula
+		output: dictionary """
+		
+		
+	
+		
+	
 
 def balance():
-	"""Takes chemical formulas of molecules from the user and creates,
+	"""Takes chemical formulas of molecules from the user,
 	balances the equation, and returns the balanced chemical equation
 	
 	Input: molecule strings in the format H2O or Al2O3
 	"""
 	
+	print("Reactants and products need to be entered in the following format:")
+	print("Water is H2O. Aluminum oxide is Al2O3\n")
 	
-	#User provides how many reactants and products
-	num_react = int(input("How many reactants are in the equation? "))
-	num_prod = int(input("How many products are in the equation? "))
 	
-	#User provides reactants, until number of reactants reaches 0
-	while num_react > 0:
+	# ~ #User provides reactants
+	# ~ reactants, products = [], []
+	# ~ while True:
+		# ~ reactant = input("Please input reactant. Input 'n' when finished. ")
+		# ~ if reactant == 'n':
+			# ~ break
+		# ~ reactants.append(reactant)
+	# ~ #User provides products
+	# ~ while True:
+		# ~ product = input("Please input product. Input 'n' when finished. ")
+		# ~ if product == 'n':
+			# ~ break
+		# ~ products.append(product)
+	
+	reactants = ['Mg', 'O2']
+	products = ['MgO']
+	
+	for item in reactants:
+		reactants.append(Formula(item))
 		
-		
-		
 	
-	
-
-
-
 
 
 if __name__ == '__main__':
