e471af4f3fc20fe3dd5ed710222954ced982508f
tobinjosh <46658226+tobinjosh@users.noreply.github.com>
2019-01-15 18:06:58 -0600

    Initial program

---
+++ equation_balancer.py
@@ -0,0 +1,38 @@
+# Program that takes chemical equations and balances them
+
+
+
+
+
+
+
+
+
+
+
+def balance():
+	"""Takes chemical formulas of molecules from the user and creates,
+	balances the equation, and returns the balanced chemical equation
+	
+	Input: molecule strings in the format H2O or Al2O3
+	"""
+	
+	
+	#User provides how many reactants and products
+	num_react = int(input("How many reactants are in the equation? "))
+	num_prod = int(input("How many products are in the equation? "))
+	
+	#User provides reactants, until number of reactants reaches 0
+	while num_react > 0:
+		
+		
+		
+	
+	
+
+
+
+
+
+if __name__ == '__main__':
+	balance()
